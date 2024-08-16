e9fbccc19f021dbef6de245c70e96631f8ac7be8
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-08-16 11:49:11 +0600

    product search added in api

--- index.js
+++ index.js
@@ -3,14 +3,78 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000
 const app = express();
+const {  MongoClient,  ServerApiVersion} = require('mongodb');
 
 app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nss4adm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // collections
+    const productCollections = client.db('baishakhiShop').collection('products')
+
+    app.get('/products', async (req, res) => {
+      const {
+        page = 1, limit = 9, keyword = '', brand = '', category = '', minPrice = 0, maxPrice = Infinity, sort = ''
+      } = req.query;
+
+      try {
+        const query = {};
+
+        // product search
+        if (keyword){
+          query.productName = {$regex: keyword, $options: 'i'};
+        }
+        
+
+        const products = await productCollections.find(query).toArray();
+
+
+        res.json({
+          products,
+          
+        });
+      } catch (err) {
+        res.status(500).json({
+          error: err.message
+        });
+      }
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({
+      ping: 1
+    });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Welcome to Baishakhi Shop")
+  res.send("Welcome to Baishakhi Shop")
 })
 
 app.listen(port, () => {
-    console.log(`Baishakhi server is running on ${port}`);
+  console.log(`Baishakhi server is running on ${port}`);
 })
