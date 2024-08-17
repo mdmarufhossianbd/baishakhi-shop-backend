021bdfcd368242756714bc091a783a52b65391b7
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-08-17 15:51:53 +0600

    sort func add

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000
 const app = express();
-const {  MongoClient,  ServerApiVersion} = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -26,44 +29,64 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // collections
-    const productCollections = client.db('baishakhiShop').collection('products')
+    const productCollection = client.db('baishakhiShop').collection('products');
+    const categoriesCollection = client.db('baishakhiShop').collection('categories');
+    const brandCollection = client.db('baishakhiShop').collection('brands')
+
+    app.use('/categoires', async (req, res) => {
+      result = await categoriesCollection.find().toArray();
+      res.json(result)
+    })
+
+    app.use('/brands', async (req, res) => {
+      result = await brandCollection.find().toArray();
+      res.json(result)
+    })
 
     app.get('/products', async (req, res) => {
       const {
-        page = 1, limit = 9, keyword = ' ', brand = '', category = '', minPrice = 0, maxPrice = Infinity, sort = ''
+        page = 1, limit = 9, keyword = '', brand = '', category = '', minPrice = 0, maxPrice = Infinity, sort = ''
       } = req.query;
 
       try {
         const query = {};
-
         // product search
-        if (keyword){
-          query.productName = {$regex: keyword, $options: 'i'};
-        }
+        if (keyword) { query.productName = { $regex: keyword, $options: 'i'}}
         // brand filtering
-        if(brand){
-          query.brandName = {$regex: brand, $options: 'i'};
-        }
+        if (brand) {query.brandName = {$regex: brand, $options: 'i'}}
         // category filtering
-        if(category){
-          query.productCategory = {$regex: category, $options: 'i'}
-        }
+        if (category) {query.productCategory = {$regex: category, $options: 'i'}}
         // price filtering
         let min = parseFloat(minPrice);
         let max = parseFloat(maxPrice);
-        if(!isNaN(min) && !isNaN(max)){
-          query.productPrice = {$gte: min, $lte: max}
-        } else if(!isNaN(min)){
-          query.productPrice = {$gte: min}
-        } else if(!isNaN(max)){
-          query.productPrice = {$lte: max}
+        if (!isNaN(min) && !isNaN(max)) {
+          query.productPrice = { $gte: min, $lte: max}
+        } else if (!isNaN(min))
+          {query.productPrice = {$gte: min}
+        } else if (!isNaN(max))
+          { query.productPrice = { $lte: max}
         }
-        const products = await productCollections.find(query).skip((page - 1) * limit).limit(parseInt(limit)).toArray();
 
+        // Sorting options
+        let sortOption = {};
+        if (sort === 'priceAsc') {
+          sortOption.productPrice = 1;
+        } else if (sort === 'priceDesc') {
+          sortOption.productPrice = -1;
+        } else if (sort === 'dateDesc') {
+          sortOption.productStoreDate = -1;
+        }
+
+        const products = await productCollection.find(query).sort(sortOption).skip((page - 1) * limit).limit(parseInt(limit)).toArray();
+        // const products = await productCollection.find(query).skip((page - 1) * limit).limit(parseInt(limit)).toArray();
+        const total = await productCollection.estimatedDocumentCount(query);
 
         res.json({
           products,
-          
+          totalPages: Math.ceil(total / limit),
+          currentPage: parseInt(page),
+          total,
+          foundProduct: products.length
         });
       } catch (err) {
         res.status(500).json({
@@ -71,6 +94,21 @@ async function run() {
         });
       }
     });
+
+    app.get('/all-products', async (req, res) => {
+      const {
+        keyword = ''
+      } = req.query
+      let query = {}
+      if (keyword) {
+        query.productName = {
+          $regex: keyword,
+          $options: 'i'
+        };
+      }
+      const result = await productCollection.find(query).toArray();
+      res.json(result)
+    })
 
 
     // Send a ping to confirm a successful connection
